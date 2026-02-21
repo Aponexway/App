1f494e7c837a78ed7839766ec3eb47fdd3ebe13b
Aponex <mishigoldentranslacii@gmail.com>
2026-02-21 22:55:08 +0300

    Убрал лишнее

---
+++ Front/sensdoc/src/components/Header.js
@@ -0,0 +1,12 @@
+function Header(){
+    return (
+        <header>
+            <div>
+                <p>SENSIBLE DOCUMENTATION</p>
+                <p>ЧИТАЙ С УДОВОЛЬСТВИЕМ</p>
+            </div>
+        </header>
+    );
+}
+
+export default Header
